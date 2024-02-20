3f8a9de2f71b0012db95a3e5e62d8e6a734d65fb
NICOLLEMON27 <142424875+NICOLLEMON27@users.noreply.github.com>
2024-02-20 13:01:35 -0500

    Add files via upload

---
+++ libCollletions/libCollletions/pkgCollletions/pkgLineal/pkgInterfaces/iStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pkgServices.pkgCollletions.pkgLineal.pkgInterfaces
+{
+    public interface iStack<T>where T:IComparable<T>
+    {
+        bool opPush(T prmItem);
+        bool opPop(ref T prmItem);
+        bool opPeek(ref T prmItem);
+
+    }
+}
---
+++ libCollletions/libCollletions/pkgCollletions/pkgLineal/pkgVector/clsVevtorStack.cs
@@ -0,0 +1,23 @@
+﻿using pkgServices.pkgCollletions.pkgLineal.pkgInterfaces;
+using System;
+
+namespace pkgServices.pkgCollletions.pkgLineal.pkgVector
+{
+    public class clsVevtorStack<T> : iStack<T> where T : IComparable<T>
+    {
+        public bool opPeek(ref T prmItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool opPop(ref T prmItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool opPush(T prmItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
